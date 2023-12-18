b939ff4327f3a2b6b990ee6bb68dd0cae5ae311e
Fil <f.visocchi@stonehex.com>
2023-12-18 14:47:54 +0100

    clean up

--- imx8m_var_reventon_bootloader-prepare.sh
+++ imx8m_var_reventon_bootloader-prepare.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-echo "#### Reventon Carrier from Stonehex, starting create image process ####"
+echo "========= Reventon Carrier from Stonehex, starting create image process ========="
 CURRENT=`pwd`
 IMAGES_PATH=$1
 UBOOT_PATH=$CURRENT/output/build/uboot-lf_v2023.04_var01
-TOOLS_PATH=$CURRENT/board/stonehex/reventon
 IMX_BOOT_TOOLS=imx-boot-tools
 REV_PKG=SthRevVar_i.mx8m_1.0.0
 cd $UBOOT_PATH
 
 if [ ! -d ${BR2_DL_DIR}/${REV_PKG} ]; then
-	echo "**** ${REV_PKG} cloning	****"
+	echo "========= ${REV_PKG} cloning ========="
 	pushd ${BR2_DL_DIR}
 	git clone https://github.com/StoneHEX/${REV_PKG}.git
 	popd
 else
-	echo "**** ${REV_PKG} present,updating	****"
+	echo "========= ${REV_PKG} present,updating	========="
 	pushd ${BR2_DL_DIR}/${REV_PKG}
 	git pull
 	popd
 fi
 if [ ! -d ${BR2_DL_DIR}/${REV_PKG} ]; then
-	echo "**** ${REV_PKG} not found	****"
+	echo "========= ${REV_PKG} not found	========="
 	exit -1
 fi
 
@@ -50,7 +49,7 @@ git apply ../meta-variscite-bsp-imx/recipes-bsp/imx-mkimage/imx-boot/0002-iMX8M-
 git apply ../meta-variscite-bsp-imx/recipes-bsp/imx-mkimage/imx-boot/0003-iMX8M-soc-add-variscite-imx8mm-support.patch
 cp iMX8M/soc.mak ..
 cd ..
-echo "**************** ATF *********************"
+echo "========= ATF ========="
 cd imx-atf 
 source ${CURRENT}/output/build/${REV_PKG}/SourceMe64
 unset LDFLAGS
@@ -60,13 +59,13 @@ cd ..
 cp ../tools/mkimage mkimage_uboot
 cp ../u-boot.bin .
 cp ../u-boot-nodtb.bin ../spl/u-boot-spl.bin ../arch/arm/dts/imx8mm-var-dart-customboard.dtb ../arch/arm/dts/imx8mm-var-som-symphony.dtb  .
-echo "**************** MKIMAGE *********************"
+echo "========= MKIMAGE ========="
 make -f soc.mak clean
 make -f soc.mak SOC=iMX8MM SOC_DIR=$IMX_BOOT_TOOLS dtbs="imx8mm-var-dart-customboard.dtb imx8mm-var-som-symphony.dtb " MKIMG=./mkimage_imx8 PAD_IMAGE=./pad_image.sh CC=gcc OUTIMG=$CURRENT/output/images/imx8-boot-sd.bin flash_lpddr4_ddr4_evk 
-echo "**************** FILE SYSTEM AND BOOT.SCR *********************"
+echo "========= FILE SYSTEM AND BOOT.SCR ========="
 cd ${CURRENT}
 output/host/usr/bin/mkimage -A arm -O linux -T ramdisk -C none -n "uInitrd" -a 0x80000 -d output/images/rootfs.ext2.gz output/images/uInitrd
 output/host/usr/bin/mkimage -C none -A arm -T script -d ${BR2_DL_DIR}/${REV_PKG}/boot_script output/images/boot.scr
-echo "#### Reventon Carrier from Stonehex , image done ####"
+echo "========= Reventon Carrier from Stonehex , image done ========="
 
 
